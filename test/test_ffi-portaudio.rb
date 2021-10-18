26ccbe07ef670f54d4be5b6e1c82ee19770a2326
kojix2 <2xijok@gmail.com>
2021-10-18 14:49:36 +0900

    Add test for FFI::PortAudio::API.Pa_GetVersionText

--- test/test_ffi-portaudio.rb
+++ test/test_ffi-portaudio.rb
@@ -4,4 +4,8 @@ class TestFFIPortaudio < Test::Unit::TestCase
   test :it_has_a_version_number do
     assert_kind_of String, ::FFI::PortAudio::VERSION
   end
+
+  test :it_get_portaudio_version_number do
+    assert_kind_of String, ::FFI::PortAudio::API.Pa_GetVersionText
+  end
 end
